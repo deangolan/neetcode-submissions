423a8d4a2bbe7508d8b96022ac8f14a0ff3fb099
deangolan <154100132+deangolan@users.noreply.github.com>
2026-05-06 23:51:24 -0700

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,21 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.prefixMin = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.prefixMin:
+            self.prefixMin.append(val)
+        else:
+            self.prefixMin.append(min(self.prefixMin[-1], val))
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.prefixMin.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.prefixMin[-1]
